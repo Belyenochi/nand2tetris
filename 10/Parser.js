d55920d53de68709fb5641df1a3e5af577cbbb8e
jason <754572491@qq.com>
2018-08-12 19:46:50 +0800

    feat(code):add project 10 Tokenizer solution

---
+++ 10/Parser.js
@@ -0,0 +1,201 @@
+/*
+    statement:     ifStatement
+                   whileStatement
+                   letStatement
+
+    statement:     statement*
+
+  ifStatement:  'if''('expression')'
+                   '{' statement '}'
+ letStatement:  'let' varName '=' expression ';'
+
+   expression:    term (op term)?
+
+         term:    varName | constant
+
+      varName:  a string not beginning with digit
+
+     constant:  a decimal number
+
+           op: '+'|'-'|'='|'>'|'<'
+ */
+let fs = require('fs');
+let os = require('os');
+
+class JackTokenizer {
+    constructor(fileStream) {
+        this.tokens = [];
+        this.input = fileStream;
+        this.currToken = '';
+        this.decode = {
+            "<" : "&lt;" ,
+            ">" : "&gt;",
+            "&" : "&amp;",
+            " " : "&nbsp;",
+            "\"": "&quot;",
+            "©" : "&copy;"
+            // Add more
+        };
+    }
+
+    hasMoreTokens() {
+        return this.input.length > 0;
+    }
+
+    advance() {
+        let match;
+
+        this.input = this.input.trim();
+        if (match = /^(class|constructor|function|method|field|static|var|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return)/.exec(this.input)) {
+            this.tokens.push({type :'keyword',value: match[0]});
+        } else if (match = /^(\{|\}|\(|\)|\[|\]|\.|\,|;|\+|\-|\*|\/|&|\||<|>|=|~)/.exec(this.input)) {
+            this.tokens.push({type :'symbol',value: match[0]});
+        } else if (match = /^(327[0-6][0-7])|^(3[012][0-6][0-9][0-9])|^([1-2]?[0-9]?[0-9]?[0-9]?[0-9])/.exec(this.input)) {
+            this.tokens.push({type :'integerConstant',value: match[0]});
+        } else if (match = /^"[^"]*"/.exec(this.input)) {
+            this.tokens.push({type :'stringConstant',value: match[0].replace(/"/, "").replace(/"/, "")});
+        } else if (match = /^\w+/.exec(this.input)) {
+            this.tokens.push({type :'identifier',value: match[0]});
+        } else {
+            throw SyntaxError("Invalid Syntax: " + this.input);
+        }
+
+        this.input = this.input.slice(match[0].length)
+        }
+
+        tokenType() {
+
+        }
+
+        keyword() {
+
+        }
+
+        symbol() {
+
+        }
+
+        identifier() {
+
+        }
+
+        intVal() {
+
+        }
+
+        StringVal() {
+
+        }
+}
+
+class CompilationEngine {
+    constructor() {
+
+    }
+
+    compileFiles() {
+
+    }
+
+    compileClassVarDec() {
+
+    }
+
+    compileSubroutine() {
+
+    }
+
+    compileParameterList() {
+
+    }
+
+    compileVarDec() {
+
+    }
+
+    compileStatements() {
+
+    }
+
+    compileDo() {
+
+    }
+
+    compileLet() {
+
+    }
+
+    compileWhile() {
+
+    }
+
+    compileReturn() {
+
+    }
+
+    compileIf() {
+
+    }
+
+    compileExpression() {
+
+    }
+
+    compileTerm() {
+
+    }
+
+    compileExpressionList() {
+
+    }
+}
+
+class JackAnalyzer {
+    constructor(inputFile, outputFile) {
+        this.input = this.clearData(this.readFileStream(inputFile));
+        this.output = outputFile;
+    }
+
+    readFileStream(filePath) {
+        let contentArray = [];
+
+        contentArray = fs.readFileSync(filePath, 'utf8').split('\n');
+
+        return contentArray;
+    }
+
+    clearData(contentArray) {
+        return contentArray.map((item) => {
+            return item.replace(/\/\/.*|\/\*\*.*/, "")
+        }).filter((item) => {
+            if (/^\s*\*/.exec(item)) {
+                return false
+            }
+            return true
+        }).join("");
+    }
+
+    outputFile() {
+        let jackToken = new JackTokenizer(this.input);
+        let fWrite = fs.createWriteStream(this.output);
+
+        while (jackToken.hasMoreTokens()) {
+            jackToken.advance();
+        }
+
+        fWrite.write("<tokens>" + os.EOL);
+        jackToken.tokens.forEach((data)=> {
+            if(jackToken.decode[data.value] && jackToken.decode.hasOwnProperty(data.value)) {
+                data = `<${data.type}> ${jackToken.decode[data.value]} </${data.type}>`
+            } else {
+                data = `<${data.type}> ${data.value} </${data.type}>`
+            }
+
+            fWrite.write(data + os.EOL);
+        });
+        fWrite.write("</tokens>" + os.EOL);
+    }
+
+}
+
+new JackAnalyzer('Square/Square.jack', 'Square/Square.xml2').outputFile();
